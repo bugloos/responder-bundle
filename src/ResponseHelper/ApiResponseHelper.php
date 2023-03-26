5de24cab65a33852bacd2f3d5f89785bfc737889
mojtaba gheytasi <mjgheytasi@gmail.com>
2023-03-26 23:35:52 +0330

    Add new response helpers and change some old ones

--- src/ResponseHelper/ApiResponseHelper.php
+++ src/ResponseHelper/ApiResponseHelper.php
@@ -5,69 +5,14 @@ namespace Bugloos\ResponderBundle\ResponseHelper;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-
-
 /**
  * @author Mojtaba Gheytasi <mjgheytasi@gmail.com>
- *
- * @method json($data, int $status = 200, array $headers = [], array $context = [])
  */
 trait ApiResponseHelper
 {
     private int $statusCode;
 
-    protected function respondWithSuccessMessage(
-        $message,
-        array $headers = []
-    ): JsonResponse {
-        return $this->setStatusCode(Response::HTTP_OK)
-            ->respondWithMessage($message, $headers);
-    }
-
-    protected function respondWithItem(
-        $item,
-        array $groups = [],
-        array $headers = []
-    ): JsonResponse {
-        return $this->setStatusCode(Response::HTTP_OK)
-            ->respond($item, $groups, $headers);
-    }
-
-    protected function respondWithCollection(
-        $collection,
-        array $groups = [],
-        array $headers = []
-    ): JsonResponse {
-        return $this->setStatusCode(Response::HTTP_OK)
-            ->respond($collection, $groups, $headers);
-    }
-
-    protected function respondWithPagination(
-        $pagination,
-        array $groups = [],
-        array $headers = []
-    ): JsonResponse {
-        return $this->setStatusCode(Response::HTTP_OK)
-            ->respond($pagination, $groups, $headers);
-    }
-
-    protected function respondItemCreated(
-        string $message,
-        array $headers = []
-    ): JsonResponse {
-        return $this->setStatusCode(Response::HTTP_CREATED)
-            ->respondWithMessage($message, $headers);
-    }
-
-    protected function respondItemUpdated(
-        $message,
-        array $headers = []
-    ): JsonResponse {
-        return $this->setStatusCode(Response::HTTP_OK)
-            ->respondWithMessage($message, $headers);
-    }
-
-    protected function respondItemDeleted(
+    protected function respondDeleted(
         string $message,
         array $headers = []
     ): JsonResponse {
@@ -82,7 +27,7 @@ trait ApiResponseHelper
     }
 
     protected function respondForbiddenError(
-        $message,
+        string $message,
         array $headers = []
     ): JsonResponse {
         return $this->setStatusCode(Response::HTTP_FORBIDDEN)
@@ -90,7 +35,7 @@ trait ApiResponseHelper
     }
 
     protected function respondNotFoundError(
-        $message,
+        string $message,
         array $headers = []
     ): JsonResponse {
         return $this->setStatusCode(Response::HTTP_NOT_FOUND)
@@ -98,12 +43,140 @@ trait ApiResponseHelper
     }
 
     protected function respondUnauthorizedError(
-        $message,
+        string $message,
         array $headers = []
     ): JsonResponse {
         return $this->setStatusCode(Response::HTTP_UNAUTHORIZED)
             ->respondWithMessage($message, $headers);
     }
+
+    protected function respondWithSuccessMessage(
+        string $message,
+        array $headers = []
+    ): JsonResponse {
+        return $this->setStatusCode(Response::HTTP_OK)
+            ->respondWithMessage($message, $headers);
+    }
+
+    /** new functions - start */
+
+    protected function respondWithSucess(
+        array|object $data,
+        array $groups= [],
+        array $headers = []
+    ): JsonResponse {
+        return $this->setStatusCode(Response::HTTP_OK)
+            ->respond($data, $groups, $headers);
+    }
+
+    protected function respondCreated(
+        array|object $data,
+        array $groups= [],
+        array $headers = []
+    ): JsonResponse {
+        return $this->setStatusCode(Response::HTTP_CREATED)
+            ->respond($data, $groups, $headers);
+    }
+
+    protected function respondCreatedMessage(
+        string $message,
+        array $headers = []
+    ): JsonResponse {
+        return $this->setStatusCode(Response::HTTP_CREATED)
+            ->respondWithMessage($message, $headers);
+    }
+
+    /** Legacy functionas - start */
+
+    /**
+     * @deprecated since Responder Bundle v1.0.0, use respondWithSucess() instead
+     */
+    protected function respondWithItem(
+        $item,
+        array $groups = [],
+        array $headers = []
+    ): JsonResponse {
+
+        trigger_deprecation('bugloos/responder-bundle', '1.0.0', 'The "%s()" method is deprecated, use "respondWithSucess()" instead.', __METHOD__);
+
+        return $this->setStatusCode(Response::HTTP_OK)
+            ->respond($item, $groups, $headers);
+    }
+
+    /**
+     * @deprecated since Responder Bundle v1.0.0, use respondWithSucess() instead
+     */
+    protected function respondWithCollection(
+        $collection,
+        array $groups = [],
+        array $headers = []
+    ): JsonResponse {
+
+        trigger_deprecation('bugloos/responder-bundle', '1.0.0', 'The "%s()" method is deprecated, use "respondWithSucess()" instead.', __METHOD__);
+
+        return $this->setStatusCode(Response::HTTP_OK)
+            ->respond($collection, $groups, $headers);
+    }
+
+    /**
+     * @deprecated since Responder Bundle v1.0.0, use respondWithSucess() instead
+     */
+    protected function respondWithPagination(
+        $pagination,
+        array $groups = [],
+        array $headers = []
+    ): JsonResponse {
+
+        trigger_deprecation('bugloos/responder-bundle', '1.0.0', 'The "%s()" method is deprecated, use "respondWithSucess()" instead.', __METHOD__);
+
+        return $this->setStatusCode(Response::HTTP_OK)
+            ->respond($pagination, $groups, $headers);
+    }
+
+    /**
+     * @deprecated since Responder Bundle v1.0.0, use respondWithSucessMessage() instead
+     */
+    protected function respondItemUpdated(
+        $message,
+        array $headers = []
+    ): JsonResponse {
+
+        trigger_deprecation('bugloos/responder-bundle', '1.0.0', 'The "%s()" method is deprecated, use "respondWithSucessMessage()" instead.', __METHOD__);
+
+        return $this->setStatusCode(Response::HTTP_OK)
+            ->respondWithMessage($message, $headers);
+    }
+
+
+    /**
+     * @deprecated since Responder Bundle v1.0.0, use respondCreated() OR respondCreatedMessage() instead
+     */
+    protected function respondItemCreated(
+        string $message,
+        array $headers = []
+    ): JsonResponse {
+
+        trigger_deprecation('bugloos/responder-bundle', '1.0.0', 'The "%s()" method is deprecated, use "respondCreated()" OR "respondCreatedMessage()" instead.', __METHOD__);
+
+        return $this->setStatusCode(Response::HTTP_CREATED)
+            ->respondWithMessage($message, $headers);
+    }
+
+    /**
+     * @deprecated since Responder Bundle v1.0.0, use respondDeleted() instead
+     */
+    protected function respondItemDeleted(
+        string $message,
+        array $headers = []
+    ): JsonResponse {
+
+        trigger_deprecation('bugloos/responder-bundle', '1.0.0', 'The "%s()" method is deprecated, use "respondDeleted()" instead.', __METHOD__);
+
+        return $this->setStatusCode(Response::HTTP_OK)
+            ->respondWithMessage($message, $headers);
+    }
+
+    /** Legacy functionas - end */
 
     private function setStatusCode(int $statusCode): self
     {
@@ -133,4 +206,5 @@ trait ApiResponseHelper
             'message' => $message,
         ], $headers, []);
     }
+
 }
